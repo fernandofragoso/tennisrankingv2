2a14c7c1c849f2c430b2eea535381b73c559bf0d
Fernando Fragoso <fernandofragoso@gmail.com>
2014-12-17 20:41:14 -0300

    Log on Database

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ var Match = require('../models/match');
 mongoose.connect('mongodb://heroku_app32612022:1aep7n66hoodpnpdsr4fpoi3g0@ds027741.mongolab.com:27741/heroku_app32612022', 
 	function(err){
 		if(err){
-			console.log("Erro no DB");
+			console.log("######## SEM CONEXÃO COM O BANCO DE DADOS!! ########");
 		}
 		// populateDB();
 	});
