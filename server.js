f82d3656b3e1a1a07df866ee010b84b60cb84b55
Fernando Fragoso <fernandofragoso@gmail.com>
2014-12-17 20:52:38 -0300

    Adapting server to Heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use('/api', require('./routes/api'));
 
 //START SERVER
-var server = app.listen(3000, function(){
+var server = app.listen((process.env.PORT || 3000), function(){
 	var host = server.address().address
 	var port = server.address().port
 
