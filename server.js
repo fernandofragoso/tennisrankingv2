0187e9fa98d0a5bfb8a3dcd1843b8271f12e272f
Fernando Fragoso <fernandofragoso@gmail.com>
2016-01-25 22:51:01 -0300

    Try cross domain again

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 //BRAGA WIN
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -52,26 +58,6 @@ auth = function(req, res, next){
 app.use('/api', require('./routes/api'));
 
 app.use('/adm', require('./routes/adm'));
-
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 //START SERVER
 var server = app.listen((process.env.PORT || 3000), function(){
