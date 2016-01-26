2f5984c9a147171ab2181f4f4578912e42812726
Fernando Fragoso <fernandofragoso@gmail.com>
2016-01-25 22:41:47 -0300

    Try cross domain

--- server.js
+++ server.js
@@ -53,6 +53,26 @@ app.use('/api', require('./routes/api'));
 
 app.use('/adm', require('./routes/adm'));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //START SERVER
 var server = app.listen((process.env.PORT || 3000), function(){
 	var host = server.address().address
